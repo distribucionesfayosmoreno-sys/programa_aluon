7b95e9e8477c394565cece65a8d58777f76178bb
rodri <rodri.moreno.moreno.82@gmail.com>
2026-05-20 06:37:03 +0200

    Revert "chore(null-safety): annotate enum list endpoints"
    This reverts commit e861b320f27bcbef12aae6136384cd5055e3fe46.

--- backend/src/main/java/com/aluon/core/user/controller/RoleController.java
+++ backend/src/main/java/com/aluon/core/user/controller/RoleController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 import com.aluon.core.user.model.Role;
-import org.eclipse.jdt.annotation.NonNull;
 
 
 @RestController
@@ -14,7 +14,7 @@ import org.eclipse.jdt.annotation.NonNull;
 public class RoleController {
 
     @GetMapping
-    public List<@NonNull Role> getRoles() {
-        return List.of(Role.values());
+    public List<Role> getRoles() {
+        return Arrays.asList(Role.values());
     }
 }
